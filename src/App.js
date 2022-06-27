9233f5024195279997487a79014477b987c44aa0
SaydAliUsmanov <usmanov.guno@gmail.com>
2022-06-27 21:15:04 +0300

    hello3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
 			asdfpokasdf
 			asdofkasdfsdfpokasdafasdfasdfpsaokdfasdfsdf
 			dsakfmaksdfmasddsffasdsddfsdfsffлотоasdkasmdk
-			lkmlkjlkdfgasd
+			lkmlkjlkdfgasdasd
 		</div>
 	);
 };
