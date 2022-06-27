8ad96443c6373393f9048441743f7efd0d82f8ce
SaydAliUsmanov <usmanov.guno@gmail.com>
2022-06-27 20:27:59 +0300

    sdfkasf

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 const App = () => {
 	return (
 		<div>
-			s;lsdkf;lsdfsf
+			s;lsdkf;lsdfsfsdfkaof
+			asdfpokasdf
+			asdofkasdfsdfpokasdfasdfpsaokdfa
 		</div>
 	);
 };
