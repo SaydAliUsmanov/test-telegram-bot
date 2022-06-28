d1a336fb483df21ac8c668e8feff1f719b2ed471
SaydAliUsmanov <usmanov.guno@gmail.com>
2022-06-28 09:27:37 +0300

    helloworld

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
 		<>
 		<div>
 			s;lsdkf;lsdfsfsdfkaof
-			asdfpokasdf,mmfsdfdsfsdfsd
+			asdfpokasdf,mmfsdfdsfsdfsddaskfnasf
 			asdofkasdfsdfpokasdafasdfasdfpsaokdfasdfsdf
 			dsakfmaksdfmasddsffasdsddfsdfsffлотоasdkasmолтdk
 			lkmlkjlkdfgasdasddsskdjsdffsdsdfflkklsdnfdfglknklkkdsflkmelkefsdasjfdns
