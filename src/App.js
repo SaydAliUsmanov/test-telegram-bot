8056e06290d6491ce27b4550a96901c9096e74dd
Muhammad <tasuhanow@mail.ru>
2022-06-30 14:58:21 +0300

    csdf

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
 	return (
 		<>
 		<div>
-			lkjlk
+			lkjlkuu
 			s;lsdkf;lsdfsfsdfkaof
 			asdfpokasdf,mmfsdfdsfsdfsddaskfnasf
 			asdofkasdfsdfpokasdafasdfasdfpsaokdfasdfssdsfdf
