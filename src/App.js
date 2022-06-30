2aee746395885c9ff1fdd9c6d550e0e2b97de758
SaydAliUsmanov <usmanov.guno@gmail.com>
2022-06-30 14:16:35 +0300

    sdfdsfksnf

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
 			asdfpokasdf,mmfsdfdsfsdfsddaskfnasf
 			asdofkasdfsdfpokasdafasdfasdfpsaokdfasdfsdf
 			dsakfmaksdfmasddsffalkjlksdsddfsdfsffлотоasdkalijsmолтdk
-			lkmlkjlkdfgasdasddsskdjsdffsdsdfflsdfkklsdnfdfkdspodkfglknklksakDMkdsflkmefdlkgjlkefsdasjfdns
+			lkmlkjlkdfgasdasddsskdjsdffsdassdfflsdfkklsdnfdfkdspodkfglknklksakDMkdsflkmefdlkgjlkefsdasjfdns
 		</div>
 		</>
 	);
